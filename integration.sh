0873c7413bd345374e5cdf29741ca2bd1a80e550
Nelson Vides <nelson.vides@erlang-solutions.com>
2019-12-03 10:24:22 +0100

    change --only integration

--- integration.sh
+++ integration.sh
@@ -6,4 +6,4 @@ docker run -p 4000:4000 -p 4001:4001 --name fcm --network n mongooseim/fcm-mock-
 
 docker run -v `pwd`/priv:/opt/app/priv -e PUSH_FCM_ENABLED=true -e PUSH_FCM_PORT=4000 -e PUSH_FCM_ENDPOINT="fcm" -e PUSH_APNS_ENABLED=false -e TLS_SERVER_CERT_VALIDATION=false -e FCM_AUTH_ENDPOINT="http://fcm:4001" --network n -p 8443:8443 mongooseim/mongoose-push:zp_integration-tests
 
-MIX_ENV=integration mix test test/api --only integration
+MIX_ENV=integration mix test test/api test/api/v3_fcm_test.ex
